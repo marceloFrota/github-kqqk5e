8a1cf0775cc991efc186224badb7282cf20bd65a
marceloFrota <marcelo.frota.costa@gmail.com>
2024-03-26 14:44:00 -0300

    Add Firestore snapshot listener for messages

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,20 @@ import { useFirebaseApp, useFirestore, useCollection } from 'vuefire';
 
 const messages2 = useCollection(collection(db, 'messages'));
 
+db.collection('messages').onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type === 'added') {
+      console.log('New city: ', change.doc.data());
+    }
+    if (change.type === 'modified') {
+      console.log('Modified city: ', change.doc.data());
+    }
+    if (change.type === 'removed') {
+      console.log('Removed city: ', change.doc.data());
+    }
+  });
+});
+
 
 
 
