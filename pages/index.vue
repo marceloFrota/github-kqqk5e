62f09e469b8b8f5ff2bd959f5f008ebc36928774
marceloFrota <marcelo.frota.costa@gmail.com>
2024-03-26 14:30:55 -0300

    Add messages2 collection to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
   <MessageSubmitter />
   <MessageDisplayer :messages="messages" />
+  {{ messages2 }}
 </template>
 
 <script setup>
+import { app, db, messagesRef } from '../server/lib/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { useFirebaseApp, useFirestore, useCollection } from 'vuefire';
+
+
+const messages2 = useCollection(collection(db, 'messages'));
+
+
+
+
 const { data: messages } = await useFetch(`/api/messages`);
 </script>
